4c8e52e4df06d85592d9dc7a0c5bb009fa76e63d
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-03-18 15:53:25 +0100

    import via deno.json

--- supabase/functions/update-pools/index.ts
+++ supabase/functions/update-pools/index.ts
@@ -1,9 +1,8 @@
-// supabase/functions/check-new-pools/index.ts
-import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.7.1'
+import { serve } from "std/http/server.ts";
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = Deno.env.get('SUPABASE_URL') ?? ''
-const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+const SUPABASE_URL = Deno.env.get('SUPABASE_URL') ?? '';
+const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '';
 
 serve(async (req) => {
   try {

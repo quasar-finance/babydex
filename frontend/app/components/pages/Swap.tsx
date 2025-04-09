8c3a40a25b8d97434afc57cce2d69f30186f5ca4
kyar1s <iris.ledesma.27@gmail.com>
2025-04-09 12:43:56 +0100

    fix: remove bottom button

--- frontend/app/components/pages/Swap.tsx
+++ frontend/app/components/pages/Swap.tsx
@@ -161,26 +161,28 @@ const SwapComponent: React.FC = () => {
               </TabContent>
             </Tabs>
           </div>
-          <div className="w-full flex flex-col gap-6  relative z-20">
-            <div className="backdrop-blur-md rounded-2xl">
-              {isConnected ? (
-                <Button
-                  fullWidth
-                  type="submit"
-                  isDisabled={isDisabled}
-                  isLoading={isLoading}
-                  className="backdrop-blur-md"
-                >
-                  {text}
-                </Button>
-              ) : (
-                <Button onPress={() => showModal(ModalTypes.connect_wallet)} fullWidth>
-                  Connect Wallet
-                </Button>
-              )}
+          {action === "swap" && (
+            <div className="w-full flex flex-col gap-6  relative z-20">
+              <div className="backdrop-blur-md rounded-2xl">
+                {isConnected ? (
+                  <Button
+                    fullWidth
+                    type="submit"
+                    isDisabled={isDisabled}
+                    isLoading={isLoading}
+                    className="backdrop-blur-md"
+                  >
+                    {text}
+                  </Button>
+                ) : (
+                  <Button onPress={() => showModal(ModalTypes.connect_wallet)} fullWidth>
+                    Connect Wallet
+                  </Button>
+                )}
+              </div>
+              <SwapInfoAccordion simulation={simulation} className="absolute w-full top-14" />
             </div>
-            <SwapInfoAccordion simulation={simulation} className="absolute w-full top-14" />
-          </div>
+          )}
         </FormProvider>
       </form>
     </div>

a199993b8020bac1128d105e6eac78a13cebd89b
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-04-10 14:28:23 +0200

    add pool setups

--- e2e_testing/mainnet/lp_and_swap.ts
+++ e2e_testing/mainnet/lp_and_swap.ts
@@ -1,6 +1,7 @@
 import deployed from "./deployed.json";
 import config from "../config.json";
 import {AstroportFactoryClient} from "../sdk/AstroportFactory.client";
+import {AstroportPairClient} from "../sdk/AstroportPair.client";
 import {getClientAndAddress} from "./lib";
 import {AstroportIncentivesClient} from "../sdk/AstroportIncentives.client";
 import { AstroportPairConcentratedClient } from "../sdk/AstroportPairConcentrated.client";
@@ -14,86 +15,57 @@ interface PoolInfo {
     liquidityToken: string;
 }
 
-async function findPoolByTokens(
-    factoryClient: AstroportFactoryClient,
-    denom1: string,
-    denom2: string
-): Promise<PoolInfo | undefined> {
-    const pairs = await factoryClient.pairs({});
-    
-    for (const pair of pairs) {
-        const assets = pair.asset_infos;
-        const isMatch = assets.every(asset => {
-            if ('native_token' in asset) {
-                return asset.native_token.denom === denom1 || 
-                       asset.native_token.denom === denom2;
-            }
-            return false;
-        });
-        
-        if (isMatch && assets.length === 2) {
-            return {
-                contractAddr: pair.contract_addr,
-                liquidityToken: pair.liquidity_token
-            };
-        }
-    }
-    
-    return undefined;
-}
-
 const main = async () => {
     const {client, address} = await getClientAndAddress();
     
     const token0 = "ubbn";
-    const token1 = "ibc/89EE10FCF78800B572BAAC7080AEFA301B5F3BBC51C5371E907EB129C5B900E7";
+    const token1 = "ibc/65D0BEC6DAD96C7F5043D1E54E54B6BB5D5B3AEC3FF6CEBB75B9E059F3580EA3";
 
     // bbn1r4x3lvn20vpls2ammp4ch5z08nge6h77p43ktl04efptgqxgl0qsxnwehd
     // bbn1yum4v0v5l92jkxn8xpn9mjg7wuldk784ctg424ue8gqvdp88qzlqjpr05x
-    const factoryClient = new AstroportFactoryClient(client, address, deployed.factory);
-    const poolAddress = "bbn1yum4v0v5l92jkxn8xpn9mjg7wuldk784ctg424ue8gqvdp88qzlqjpr05x";
-        const poolClient = new AstroportPairConcentratedClient(client, address, poolAddress);
+    const poolAddress = "bbn1qjn06jt7zjhdqxgud07nylkpgnaurq6x6vad38vztwxec4rr5ntsnn4dd3";
+        const poolClient = new AstroportPairClient(client, address, poolAddress);
 
-        const baby_usdc = 0.07526
-        const btc_usd = 81611;
-        const baby_btc = baby_usdc / btc_usd
-        const baby_btc_decimals = Number(baby_btc).toFixed(10);
-        console.log('Baby BTC price:', baby_btc_decimals);
+        // const baby_usdc = 0.07526
+        // const btc_usd = 81611;
+        // const baby_btc = baby_usdc / btc_usd
+        // const baby_btc_decimals = Number(baby_btc).toFixed(10);
+        // console.log('Baby BTC price:', baby_btc_decimals);
 
-        const assets = [
-            {
-                info: {
-                    native_token: {
-                        denom: token0
-                    }
-                },
-                amount: (5 * 1e6).toString() // 2 UBN with 6 decimals
-            },
-            {
-                info: {
-                    native_token: {
-                        denom: token1
-                    }
-                },
-                amount: Math.floor(40 * baby_btc * 1e8).toString() // 2 * baby_btc with 8 decimals
-            },
-        ];
-        console.log('Asset amounts:', assets);
-        const coins: Coin[] = assets.map(asset => {
-            const decimals = asset.info.native_token.denom === token0 ? 6 : 8;
-            const microAmount = asset.amount; // Already in micro-denom
-            console.log(`Converting ${asset.info.native_token.denom}: ${asset.amount} (micro-denom)`);
-            return coin(microAmount, asset.info.native_token.denom);
-        }).sort((a, b) => a.denom.localeCompare(b.denom));
-        console.log('Final coins:', coins);
+        // const assets = [
+        //     {
+        //         info: {
+        //             native_token: {
+        //                 denom: token0
+        //             }
+        //         },
+        //         amount: (5 * 1e6).toString() // 2 UBN with 6 decimals
+        //     },
+        //     {
+        //         info: {
+        //             native_token: {
+        //                 denom: token1
+        //             }
+        //         },
+        //         amount: Math.floor(5 * baby_btc * 1e8).toString() // 2 * baby_btc with 8 decimals
+        //     },
+        // ];
+        // console.log('Asset amounts:', assets);
+        // const coins: Coin[] = assets.map(asset => {
+        //     const decimals = asset.info.native_token.denom === token0 ? 6 : 8;
+        //     const microAmount = asset.amount; // Already in micro-denom
+        //     console.log(`Converting ${asset.info.native_token.denom}: ${asset.amount} (micro-denom)`);
+        //     return coin(microAmount, asset.info.native_token.denom);
+        // }).sort((a, b) => a.denom.localeCompare(b.denom));
+        // console.log('Final coins:', coins);
         
-        const response = await poolClient.provideLiquidity(
-            {assets, autoStake: true},
-            "auto",
-            undefined,
-            coins
-        );
-        console.log("Provide liquidity response:", response);
+        // const response = await poolClient.provideLiquidity(
+        //     {assets, autoStake: true},
+        //     "auto",
+        //     undefined,
+        //     coins
+        // );
+        // console.log("Provide liquidity response:", response);
 
         const swapOperation = {
             offer_asset_info: {
@@ -114,13 +86,13 @@ const main = async () => {
         try {
             const response = await routerClient.executeSwapOperations(
                 {
-                    maxSpread: "0.05",
+                    maxSpread: "0.5",
                     operations: [swapOperation],
                     to: address
                 },
                 "auto",
                 undefined,
-                [coin((0.00001 * 1e6).toString(), swapOperation.offer_asset_info.native_token.denom)]
+                [coin(70000, swapOperation.offer_asset_info.native_token.denom)]
             );
     
             console.log("Swap transaction successful:", response.transactionHash);

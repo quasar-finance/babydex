df3496ee3aaa6ac68b14ece96653aaff824d9e43
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-04-10 10:16:57 +0200

    format decimals according to token

--- frontend/app/components/molecules/DoubleSideAddLiquidity.tsx
+++ frontend/app/components/molecules/DoubleSideAddLiquidity.tsx
@@ -152,8 +152,8 @@ export const DoubleSideAddLiquidity: React.FC<Props> = ({ pool, submitRef }) => 
           <div
             className="flex gap-1 items-center cursor-pointer"
             onClick={() => {
-              setValue(token0.symbol, formatDecimals(t0DenomBalance), { shouldValidate: true });
-              setValue(token1.symbol, formatDecimals(t0DenomBalance * optimalRatio, 6), {
+              setValue(token0.symbol, formatDecimals(t0DenomBalance, token0.decimals), { shouldValidate: true });
+              setValue(token1.symbol, formatDecimals(t0DenomBalance * optimalRatio, token1.decimals), {
                 shouldValidate: true,
               });
             }}
@@ -187,7 +187,7 @@ export const DoubleSideAddLiquidity: React.FC<Props> = ({ pool, submitRef }) => 
               const regex = /^\d+(\.\d{0,18})?$/;
               if (target.value === "" || regex.test(target.value)) {
                 setValue(token1.symbol, target.value, { shouldValidate: true });
-                setValue(token0.symbol, formatDecimals(Number(target.value) / optimalRatio, 6), {
+                setValue(token0.symbol, formatDecimals(Number(target.value) / optimalRatio, token0.decimals), {
                   shouldValidate: true,
                 });
               }
@@ -199,7 +199,7 @@ export const DoubleSideAddLiquidity: React.FC<Props> = ({ pool, submitRef }) => 
             className="flex gap-1 items-center cursor-pointer"
             onClick={() => {
               setValue(token1.symbol, t1DenomBalance, { shouldValidate: true });
-              setValue(token0.symbol, formatDecimals(Number(t1DenomBalance) / optimalRatio, 6), {
+              setValue(token0.symbol, formatDecimals(Number(t1DenomBalance) / optimalRatio, token0.decimals), {
                 shouldValidate: true,
               });
             }}

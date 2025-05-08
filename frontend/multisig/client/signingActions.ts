b17fb92d07af3bc3adaa07e5c10087a773d61666
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-05-08 10:22:06 +0200

    transaction result display wip

--- frontend/multisig/client/signingActions.ts
+++ frontend/multisig/client/signingActions.ts
@@ -7,19 +7,20 @@ import type {
     CometBftRpcSchema,
     Transport,
     TxMessage,
-  } from 'cosmi/types'
+    MsgExecuteContract
+} from 'cosmi/types'
 import { toUtf8 } from 'cosmi/utils';
 
-  export type SigningActions<
-  _transport extends Transport = Transport,
-  _chain extends Chain | undefined = Chain | undefined,
-  _account extends Account | undefined = Account | undefined,
+export type SigningActions<
+_transport extends Transport = Transport,
+_chain extends Chain | undefined = Chain | undefined,
+_account extends Account | undefined = Account | undefined,
 > = {
     // TODO change these actions up to account for the multisig return type
-  execute: (args: ExecuteParameters) => ExecuteReturnType
-  executeMultiple: (
-    args: ExecuteMultipleParameters,
-  ) => ExecuteReturnType
+    execute: (args: ExecuteParameters) => ExecuteReturnType
+    executeMultiple: (
+        args: ExecuteMultipleParameters,
+    ) => ExecuteReturnType
 }
 
 export function signingActions(client: Client): SigningActions {
@@ -40,19 +41,36 @@ export async function execute(client: Client, parameters: ExecuteParameters): Ex
     });
 }
 
-// TODO return the transaction in multisig signable format
 export async function executeMultiple(client: Client, parameters: ExecuteMultipleParameters): ExecuteReturnType {
-    console.log("trying to execute something")
     const { sender, execute, gasLimit, memo, timeoutHeight } = parameters;
-    console.log("return nothing")
-    // TODO format the messages in the multisig format, simulate the gas, add it to the tx and return the total format
-    // const msgs = execute.map(({ address, message, funds }) => ({
-    //     typeUrl: MsgExecuteContract.typeUrl,
-    //     value: MsgExecuteContract.encode({
-    //         sender,
-    //         contract: address,
-    //         msg: toUtf8(JSON.stringify(message)),
-    //         funds: funds || [],
-    //     }).finish(),
-    // }));
+    
+    // Format the messages
+    const msgs = execute.map(({ address, message, funds }) => ({
+        typeUrl: MsgExecuteContract.typeUrl,
+        value: MsgExecuteContract.encode({
+            sender,
+            contract: address,
+            msg: toUtf8(JSON.stringify(message)),
+            funds: funds || [],
+        }).finish(),
+    }));
+
+    // Create the transaction object
+    const transaction = {
+        messages: msgs,
+        signer: sender,
+        gasLimit,
+        memo,
+        timeoutHeight,
+    };
+
+    // Show the transaction in a modal
+    // We'll need to use a state management solution to show the modal
+    // For now, let's use a custom event
+    const event = new CustomEvent('showTransaction', { 
+        detail: { transaction } 
+    });
+    window.dispatchEvent(event);
+
+    return transaction;
 }

c413e815571ada1cffc1082f2d2302300302bba7
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2025-05-09 12:50:25 +0200

    refactor dex actions

--- frontend/actions/addLiquidity.ts
+++ frontend/actions/addLiquidity.ts
@@ -1,7 +1,9 @@
 import { executeMultiple, type ExecuteReturnType } from "cosmi/client";
 import type { Chain, Account, CometBftRpcSchema, Transport } from "cosmi/types";
 import type { ClientWithActions } from "~/multisig/client/types";
-import { buildAddLiquidityMsg, type AddLiquidityMsgParams } from "~/actions/utils/addLiquidityMsg";
+import { setInnerValueToAsset } from "@towerfi/trpc";
+import { Currency } from "@towerfi/types";
+import { buildIncreaseAllowanceMsg } from "./increaseAllowance";
 
 export type AddLiquidityParameters = AddLiquidityMsgParams;
 export type AddLiquidityReturnType = ExecuteReturnType;
@@ -14,9 +16,72 @@ export async function addLiquidity<
   parameters: AddLiquidityParameters,
 ): AddLiquidityReturnType {
   const { sender, execMsgs } = buildAddLiquidityMsg(parameters);
-  // TODO, make this call differentiate between cosmi base signing call and our multisig signing call
   return await client.executeMultiple({
     execute: execMsgs,
     sender,
   });
 }
+
+export type AddLiquidityMsgParams = {
+  sender: string;
+  poolAddress: string;
+  autoStake?: boolean;
+  minLpToReceive?: string;
+  receiver?: string;
+  slipageTolerance: string;
+  assets: {
+    amount: string;
+    info: Currency;
+  }[];
+};
+
+export function buildAddLiquidityMsg({
+  sender,
+  poolAddress,
+  slipageTolerance,
+  autoStake,
+  minLpToReceive,
+  assets,
+  receiver,
+}: AddLiquidityMsgParams) {
+  // TODO potential improvement would we to get allowance and subtract
+
+  // Create increase allowance messages for CW20 assets
+  const cw20Assets = assets.filter(asset => asset.info.type === "cw-20");
+  const increaseAllowanceMsgs = cw20Assets.map(asset => 
+    buildIncreaseAllowanceMsg({
+      sender,
+      tokenAddress: asset.info.denom,
+      spender: poolAddress,
+      amount: asset.amount,
+    })
+  );
+
+  const provideLiquidityMsg = {
+    address: poolAddress,
+    message: {
+      provide_liquidity: {
+        receiver,
+        auto_stake: autoStake,
+        min_lp_to_receive: minLpToReceive,
+        assets: assets.map(({ info, amount }) => ({
+          info: setInnerValueToAsset(info),
+          amount,
+        })),
+        slippage_tolerance: slipageTolerance,
+      },
+    },
+    funds: assets
+      .filter((a) => a.info.type !== "cw-20")
+      .map(({ info, amount }) => ({
+        denom: info.denom,
+        amount,
+      })),
+  };
+
+  return {
+    sender,
+    execMsgs: [...increaseAllowanceMsgs.map(msg => msg.execMsg), provideLiquidityMsg],
+  };
+}
+

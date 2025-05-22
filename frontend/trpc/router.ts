ac45e1073c27927b98a393d34bfa867b7832b86a
Ron Lobo <ron@tower.fi>
2025-05-22 12:30:25 +0700

    Add correct Omit

--- frontend/trpc/router.ts
+++ frontend/trpc/router.ts
@@ -1,9 +1,20 @@
-import { initTRPC } from "@trpc/server";
+import {
+  DataTransformerOptions, DefaultDataTransformer,
+  DefaultErrorShape,
+  initTRPC,
+  RootConfig,
+  Unwrap,
+  WithoutIndexSignature
+} from "@trpc/server";
 import { observable } from "@trpc/server/observable";
 import { TRPCClientError, type TRPCLink } from "@trpc/client";
 
 import type { AnyRouter } from "@trpc/server";
 import type { ContextOptions } from "@towerfi/trpc";
+import {RouterDef} from "@trpc/server/dist/core/router";
+import {ErrorFormatterShape} from "@trpc/server/dist/error/formatter";
+import {PickFirstDefined} from "@trpc/server/dist/core/internals/utils";
+import {RuntimeConfig} from "@trpc/server/dist/core/internals/config";
 
 export const createInnerTRPCContext = (opts: ContextOptions) => {
   return opts;
@@ -18,7 +29,7 @@ export const createCallerFactory = t.createCallerFactory;
 export const createTRPCPublicProcedure = t.procedure;
 
 export function createLocalTRPCLink<TRouter extends AnyRouter>(
-  opts: { router: TRouter } & Omit<ContextOptions, "indexerService">,
+  opts: { router: TRouter } & Omit<ContextOptions, "indexerService"> & Omit<ContextOptions, "referralService">,
 ): TRPCLink<TRouter> {
   const { router, ...ctx } = opts;
   const createCaller = createCallerFactory(router);

b79f7e8d1bed2df662817506d3b7f12ba81d8104
Ron Lobo <ron@tower.fi>
2025-05-22 13:16:32 +0700

    Fix Omit the right way

--- frontend/trpc/router.ts
+++ frontend/trpc/router.ts
@@ -29,7 +29,7 @@ export const createCallerFactory = t.createCallerFactory;
 export const createTRPCPublicProcedure = t.procedure;
 
 export function createLocalTRPCLink<TRouter extends AnyRouter>(
-  opts: { router: TRouter } & Omit<ContextOptions, "indexerService"> & Omit<ContextOptions, "referralService">,
+  opts: { router: TRouter } & Omit<ContextOptions, "indexerService" | "referralService">,
 ): TRPCLink<TRouter> {
   const { router, ...ctx } = opts;
   const createCaller = createCallerFactory(router);

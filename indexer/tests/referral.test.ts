34f69a80fdcb093a6bc8b930973b6f6311d8401e
Ron Lobo <ron@tower.fi>
2025-06-04 15:08:18 +0700

    Additional cases for not allowing referrals

--- indexer/tests/referral.test.ts
+++ indexer/tests/referral.test.ts
@@ -85,15 +85,15 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await deleteReferralCode();
+  await deleteReferralCodes();
   await deleteReferrals();
 });
 
-async function deleteReferralCode() {
+async function deleteReferralCodes() {
   const { error } = await supabase
     .from("user_referral_codes")
     .delete()
-    .eq("user_wallet_address", TEST_USER_WALLET_ADDRESS);
+    .or(`user_wallet_address.eq.${TEST_USER_WALLET_ADDRESS}, user_wallet_address.eq.${TEST_REFERRED_USER_WALLET_ADDRESS}`);
 
   expect(error).toBeNull;
 }
@@ -271,6 +271,22 @@ test("handleReferral returns an error if users try to refer themselves", async (
   );
   expect(result.success).toBe(false);
   expect(result.error).toBe("User cannot refer to themselves.");
+});
+
+test('handleReferral returns an error if referred user already created referral code', async () => {
+  const store_result = await referralService.storeReferralCode(TEST_USER_WALLET_ADDRESS, mockSignedMessage);
+
+  expect(store_result.success).toBe(true);
+
+  (pubkeyToAddress as Mock).mockReturnValue(TEST_REFERRED_USER_WALLET_ADDRESS);
+
+  const store_referral_result = await referralService.storeReferralCode(TEST_REFERRED_USER_WALLET_ADDRESS, mockSignedMessage);
+
+  expect(store_referral_result.success).toBe(true);
+
+  const result = await referralService.handleReferral(TEST_REFERRED_USER_WALLET_ADDRESS, store_result.code, mockSignedMessage);
+  expect(result.success).toBe(false);
+  expect(result.error).toBe('User has already interacted with the DEX and created a referral code.');
 });
 
 // Test suite for verifyCosmosSignature

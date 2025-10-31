e7a35d9ca536138f78c78341f19dc9d780919228
dev_waliba <63817190+williamslsy@users.noreply.github.com>
2025-10-31 11:52:15 +0100

    remove banner from header

--- frontend/app/components/organisms/layout/Header.tsx
+++ frontend/app/components/organisms/layout/Header.tsx
@@ -1,51 +1,25 @@
-import Link from "next/link";
-import { NavLinks } from "./NavLinks";
-import { MenuMobile } from "./MenuMobile";
-import { useState } from "react";
-import { ConnectWallet } from "../../molecules/ConnectWallet";
-import { Hamburguer } from "../../atoms/Hamburguer";
-import { useMediaQuery } from "~/app/hooks";
-import { IconCircleDot } from "@tabler/icons-react";
+import Link from 'next/link';
+import { NavLinks } from './NavLinks';
+import { MenuMobile } from './MenuMobile';
+import { useState } from 'react';
+import { ConnectWallet } from '../../molecules/ConnectWallet';
+import { Hamburguer } from '../../atoms/Hamburguer';
+import { useMediaQuery } from '~/app/hooks';
 
 export const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const isLg = useMediaQuery("lg");
+  const isLg = useMediaQuery('lg');
   return (
     <>
-      <nav className="sticky top-0 w-full border-b-1 border-b-white/10 pb-4 backdrop-blur-lg z-50 ">
-        <div className="w-full text-[#0F0F0F] bg-tw-orange-400 flex items-center justify-center gap-3 sm:gap-1 px-2 py-1 sm:py-2 mb-4">
-          <span>
-            Our Points Referral Campaign is now{" "}
-            <span className="animate-pulse text-tw-green-700 font-bold">LIVE</span>
-          </span>
-          <div className="animate-pulse text-tw-green-700 hidden sm:block">
-            <IconCircleDot />
-          </div>
-          <a
-            href="https://docs.tower.fi/user-guides/points-campaigns"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:underline text-sm font-bold"
-          >
-            Learn More
-          </a>
-        </div>
+      <nav className="sticky top-0 w-full border-b-1 border-b-white/10 p-4 backdrop-blur-lg z-50 ">
         <div className="flex gap-6 items-center justify-between max-w-[84.5rem] mx-auto px-4">
           <div className="flex gap-10 items-center justify-center">
             <Link href="/">
-              <img
-                className="max-h-[2.3rem] object-cover"
-                src="/towerfi-logo.svg"
-                alt="towerfi-logo"
-              />
+              <img className="max-h-[2.3rem] object-cover" src="/towerfi-logo.svg" alt="towerfi-logo" />
             </Link>
             {isLg && <NavLinks />}
           </div>
-          {isLg ? (
-            <ConnectWallet />
-          ) : (
-            <Hamburguer isOpen={menuOpen} onClick={() => setMenuOpen(!menuOpen)} />
-          )}
+          {isLg ? <ConnectWallet /> : <Hamburguer isOpen={menuOpen} onClick={() => setMenuOpen(!menuOpen)} />}
         </div>
       </nav>
       {isLg ? null : <MenuMobile open={menuOpen} setIsOpen={setMenuOpen} />}

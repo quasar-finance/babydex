d8436971406f87189c6ace21f24171168a9715e9
dev_waliba <63817190+williamslsy@users.noreply.github.com>
2025-11-12 15:01:23 +0100

    updated the Overview component to accept totalPoints

--- frontend/app/components/pages/Points.tsx
+++ frontend/app/components/pages/Points.tsx
@@ -1,25 +1,23 @@
 'use client';
 
-import { Button } from '../atoms/Button';
-import { DefaultPoints } from '~/utils/consts';
 import { Overview } from '../organisms/points/Overview';
 import { trpc } from '~/trpc/client';
 import { useAccount } from '@cosmi/react';
 import WithConnectedWallet from '../atoms/WithConnectedWallet';
 import type React from 'react';
 import { useMemo } from 'react';
-import Link from 'next/link';
 import AirdropStatus from '../organisms/points/AirdropStatus';
+import airdropData from '~/constants/airdrop_final.json';
 
-const GetPointsButton: React.FC<{ className: string }> = ({ className }) => (
-  <Button as={Link} color="tertiary" className={className} target="_blank" href="https://docs.tower.fi/user-guides/points-campaigns">
-    Get Points Now
-  </Button>
-);
+type AirdropRecord = {
+  points_after_boost?: number;
+};
+
+const typedAirdropData = airdropData as Record<string, AirdropRecord>;
 
 const Points: React.FC = () => {
   const { address: userAddress } = useAccount();
-  const { data: userPoints, isLoading: _userPointsLoading } = trpc.edge.indexer.getPoints.useQuery(
+  const { data: userPoints } = trpc.edge.indexer.getPoints.useQuery(
     {
       addresses: [userAddress || ''],
       limit: 1,
@@ -29,7 +27,21 @@ const Points: React.FC = () => {
     }
   );
 
-  const userPointsData = useMemo(() => userPoints?.[userAddress || ''] || DefaultPoints(), [userPoints, userAddress]);
+  const totalPoints = useMemo(() => {
+    if (!userAddress) {
+      return undefined;
+    }
+
+    const normalizedAddress = userAddress.toLowerCase();
+    const record = typedAirdropData[normalizedAddress];
+
+    if (record && typeof record.points_after_boost === 'number') {
+      return record.points_after_boost;
+    }
+
+    const pointsFromIndexer = userPoints?.[normalizedAddress]?.total_points;
+    return typeof pointsFromIndexer === 'number' ? pointsFromIndexer : 0;
+  }, [userAddress, userPoints]);
 
   return (
     <div className="flex flex-col gap-8 px-4 pb-20 max-w-[84.5rem] mx-auto w-full min-h-[65vh] lg:pt-8">
@@ -46,7 +58,7 @@ const Points: React.FC = () => {
           </div>
           <div className="mt-4 lg:mt-8">
             <WithConnectedWallet>
-              <Overview points={userPointsData} />
+              <Overview totalPoints={totalPoints} />
             </WithConnectedWallet>
           </div>
         </div>
